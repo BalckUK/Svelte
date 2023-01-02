6622597026c3aa824c3475bfc71a8747ec3c5f57
UMK <haga5032@naver.com>
2023-01-02 14:30:55 +0900

    svelte 최초 커밋

---
+++ svelte-app/src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+    let name = 'world';
+    let age = 85
+    /* setTimeout(() => {
+        name = 'Heropy'
+        age = 36
+    }, 1000) */
+
+    function assign() {
+        name = 'Heropy'
+        age = 36
+    }
+</script>
+
+<h1>Hello {name}!</h1>
+<h2 class={age < 85 ? 'active' : ''}>
+    {age}
+</h2>
+<img src="" alt={name}/>
+<input type="text" bind:value="{name}"/>
+<button on:click={assign}>Assign</button>
+
+<style>
+    h1{
+        color : red;
+    }
+    .active{
+        color : blue;
+    }
+</style>
